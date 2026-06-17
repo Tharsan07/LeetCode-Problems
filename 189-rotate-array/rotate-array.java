32a6015342f0bfe9f66a1f5a4f2cf77e09b30041
Tharsan S <166637132+Tharsan07@users.noreply.github.com>
2026-06-17 21:34:24 +0530

    Time: 1 ms (41.23%) | Memory: 63.4 MB (8.06%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,27 +1,22 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int n = nums.length;
-      k=k%n;
-
-      int[] res=new int[n];
-      for(int i=0;i<n;i++){
-        res[(i+k)%n]=nums[i];
-      }
-      for(int i=0;i<n;i++){
-        nums[i]=res[i];
-      }
+        int len = nums.length;
+        k = k % len;
+        int rotate = len - k;
+        //Move the first rotate time values to another array
+        int[] temp = new int[rotate];
+        for(int i =0; i< rotate;i++){
+            temp[i]=nums[i];
         }
-//     int n=nums.length;
-//           k = k % n;
-
-//  for(int r=0;r<k;r++){
-//     int last=nums[n-1];
-//     for(int i=n-1;i>0;i--){
-//      nums[i]=nums[i-1];
-//     }
-//     nums[0]=last;
-//  }
+        int curr=0;
+        for (int i=rotate;i<len;i++){
+            nums[curr]= nums[i];
+            curr++;
+        }
+        for(int i =0;i < temp.length;i++){
+            nums[curr++]=temp[i];
+        }
     
-    
-
+        
+    }
 }
