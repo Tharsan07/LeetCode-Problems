2466e6b7950c97669899366ffcd79d74ede126bc
Tharsan S <166637132+Tharsan07@users.noreply.github.com>
2026-06-18 21:44:29 +0530

    Time: 2 ms (91.95%) | Memory: 47.6 MB (75.09%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,25 +1,26 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int n=nums.length;
-
-        ArrayList<Integer> list = new ArrayList<>();
-        //store all the non zero elements in the list
-        for(int i =0;i<n;i++){
-            if(nums[i]!=0){
-                list.add(nums[i]);
+            int ind =-1;
+            // First find the non zero C
+            for(int i =0 ;i<n;i++){
+                if(nums[i]==0){
+                    ind = i;
+                    break;
+                }
             }
-        }
 
-        //Move all the non zero elements to the array
-        int len = list.size();
-        for(int i =0;i<len;i++){
-            nums[i]=list.get(i);
-        }
+if (ind==-1 )return;
+            // Start from the first zero elements and swap
 
-        // Add the zero to remaining places
-       
-        for(int i = len; i<n;i++){
-            nums[i]=0;
-        }
+            for(int i =ind+1; i<n;i++){
+                if(nums[i]!=0){
+                    int temp=nums[i];
+                    nums[i]=nums[ind];
+                    nums[ind]=temp;
+                    ind++;
+                }
+            }
+
     }
 }
