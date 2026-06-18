f450a4c08730b0c48f77d3c5fa34af944eaac39d
Tharsan S <166637132+Tharsan07@users.noreply.github.com>
2026-06-18 21:45:30 +0530

    Time: 2 ms (91.95%) | Memory: 47.7 MB (56.35%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,26 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n=nums.length;
-            int ind =-1;
-            // First find the non zero C
-            for(int i =0 ;i<n;i++){
-                if(nums[i]==0){
-                    ind = i;
-                    break;
-                }
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
             }
-
-if (ind==-1 )return;
-            // Start from the first zero elements and swap
-
-            for(int i =ind+1; i<n;i++){
-                if(nums[i]!=0){
-                    int temp=nums[i];
-                    nums[i]=nums[ind];
-                    nums[ind]=temp;
-                    ind++;
-                }
-            }
-
+        }        
     }
 }
